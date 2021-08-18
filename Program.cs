c60d27dc36534551e60ad413c99dba828ec4f7b2
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-18 09:14:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EX09BooleanIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 3;
+            int x;
+
+            if (a + b == b) 
+            {
+                x = 1;
+            }
+            else
+            {
+                if (b - a == a) //True
+                {
+                    x = 2;
+                    Console.WriteLine();
+                    Console.WriteLine(x);
+                }
+                if (c - b == b)
+                {
+                    x = 3;
+                    Console.WriteLine();
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
